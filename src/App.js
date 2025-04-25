2ba85932f9648a625e80fd7054c0af50fbc76d96
Daniel Allen <danoitguy@hotmail.com>
2025-04-25 13:53:01 -0700

    Backup local changes

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+// Asked to delete in first week >>> import counter>> import logo
+// import { Counter } from './features/counter/Counter';
+// import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        hello nucamp!
+    </div>
+  );
+}
+
+export default App;
