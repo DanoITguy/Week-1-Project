2eee6575fc55194982787c58923601fa1c63e465
Daniel Allen <danoitguy@hotmail.com>
2025-04-26 17:16:22 -0700

    Add existingproject files to Git

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
-import React from 'react';
-
-
-// Asked to delete in first week >>> import counter>> import logo
-// import { Counter } from './features/counter/Counter';
-// import logo from './logo.svg';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import CampsitesList from './features/campsites/CampsitesList';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-        hello nucamp!
-    </div>
-  );
+    return (
+        <div className='App'>
+            <Header />
+            <CampsitesList />
+            <Footer />
+        </div>
+    );
 }
 
 export default App;
